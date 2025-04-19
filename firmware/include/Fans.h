547d70c4d5e6cc6cb8497f4927d12dd44f1a0569
Mikhail Khrenov <mkhrenov34@gmail.com>
2025-04-19 14:47:06 -0400

    Comments to remember  which GPIOs map to which pins

--- firmware/include/Fans.h
+++ firmware/include/Fans.h
@@ -1,9 +1,9 @@
 #include <DShotESC.h>
 
 namespace fans {
-    #define GPIO_FAN_CENTER GPIO_NUM_7
-    #define GPIO_FAN_LEFT   GPIO_NUM_8
-    #define GPIO_FAN_RIGHT  GPIO_NUM_9
+    #define GPIO_FAN_CENTER GPIO_NUM_7 // D4
+    #define GPIO_FAN_LEFT   GPIO_NUM_8 // D5
+    #define GPIO_FAN_RIGHT  GPIO_NUM_9 // D6
 
     #define THRUST_CONSTANT 0.005     // N / u
     #define FAN_UPDATE_PERIOD_US 100
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -201,7 +201,7 @@ void loop()
         }
 
         strip.show();
-        imus::print();
+        // imus::print();
     }
     // Update outputs
     fans::update(theta);
